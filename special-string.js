4349f0e20397f0c52c071ce3c158ab184a7f9609
Christi Kerr <christinakerr394@gmail.com>
2021-02-22 10:48:23 -0600

    Solution for special string again

--- special-string.js
+++ special-string.js
@@ -1,3 +1,29 @@
 function substrCount(n, s){
-    
+    let result = 0;
+    let i = 0;
+
+    while (i < n){
+        let charCount = 1;
+        while (i + 1 < s.length && s[i] == s[i+1]){
+            i++;
+            charCount++;
+        }
+        result += parseInt((charCount * (charCount + 1)) / 2);
+        i++
+    }
+
+    for (i = 1; i < n; i++){
+        let charCount = 1;
+        while (
+            i + charCount < s.length &&
+            i - charCount >= 0 &&
+            s[i-1] != s[i] &&
+            s[i-1] == s[i-charCount] &&
+            s[i-1] == s[i+charCount]
+        ) {
+            charCount++;
+        }
+        result += charCount-1;
+    }
+    return result;
 }
